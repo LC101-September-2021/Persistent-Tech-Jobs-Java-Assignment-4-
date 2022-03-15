e21ad8bc57758468f32b9ea52cdc42e70021070a
bunny-mack <bmcintyre.133@gmail.com>
2022-03-14 20:01:58 -0500

    Part 2 Working (reference Creating a Many-to-One Relationship)

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @OneToOne
+    @NotBlank(message = "Skill is required")
+    @Size(max = 150, message = "Description too long")
+    private String skill;
+
+    public Skill(String skill) {
+        this.skill = skill;
+    }
+
+    public Skill(){}
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
 }
